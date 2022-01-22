2709a1e6ce0a48660ca0cac789d246afb79c491d
syler11 <nemeth.szilard82@gmail.com>
2022-01-22 09:27:38 +0000

    Configured apppy for flask and mongo

--- app.py
+++ app.py
@@ -9,3 +9,14 @@ if os.path.exists("env.py"):
 
 
 app = Flask(__name__)
+
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
