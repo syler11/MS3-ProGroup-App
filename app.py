6a306851e96dac2f66e1e614f37d99ecba8bc3ce
syler11 <nemeth.szilard82@gmail.com>
2022-01-22 13:09:23 +0000

    css and js linked jquery loaded

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
