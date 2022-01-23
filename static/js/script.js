6c74f43d2a275187c4fc53ec3229d9d60bb45028
syler11 <nemeth.szilard82@gmail.com>
2022-01-23 09:24:51 +0000

    date and time is displayed with js on base.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,36 @@
-
-    var today = new Date();
-    var hourNow = today.getHours();
-    var greeting;
-
-    if (hourNow > 18) {
-        greeting = "Good evening, ";
-    } else if (hourNow > 12) {
-        greeting = "Good afternoon, ";
-    } else if (hourNow > 0) {
-        greeting = "Good morning, ";
-    } else {
-        greeting = "Welcome"
-    }
-
-    document.getElementById("greeting").innerHTML = greeting + 'Szilard';
-
-
-
 $(document).ready(function() {
     $("#copyright").text(new Date().getFullYear());
-    $('.dropdown-trigger').dropdown();
 });
 
+// Display full date and greets the session user accoring to teh time of the day
+function dateGreeting() {
+var today = new Date();
+var day = today.getDate();
+var month = today.getMonth();
+var year = today.getFullYear();
+var hour = today.getHours();
+var greeting;
+
+if (hour > 18) {
+    greeting = "Good evening, ";
+} else if (hour > 12) {
+    greeting = "Good afternoon, ";
+} else if (hour > 0) {
+    greeting = "Good morning, ";
+} else {
+    greeting = "Welcome"
+}
+
+document.getElementById("greeting").innerHTML = greeting + 'Szilard';
+document.getElementById("txtDate").innerHTML = 'Date: ' + day + '.' + month+1 + '.' + year;
+}
+
+// Dispaly counting clock
+window.onload = displayClock();
+function displayClock(){
+  var clock = new Date().toLocaleTimeString();
+  document.getElementById("txtTime").innerHTML = 'Time: ' + clock;
+  setTimeout(displayClock, 1000); 
+}
+
+dateGreeting();
