835b9fec6e2b0c09130c56b760669d17a94aa9c5
syler11 <nemeth.szilard82@gmail.com>
2022-01-23 10:40:10 +0000

    Reservation html added

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def login():
-    return render_template("base.html")
+    return render_template("reservations.html")
 
 
 if __name__ == "__main__":
