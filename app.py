7a2de51eacf7f7a4dc8673b2da0c7630662c1c45
syler11 <nemeth.szilard82@gmail.com>
2022-01-28 21:22:04 +0000

    edit reservation function added in app.py

--- app.py
+++ app.py
@@ -56,9 +56,44 @@ def add_reservation():
         mongo.db.reservations.insert_one(reservations)
         return redirect(url_for("reservations"))
     
-    profiles = mongo.db.profiles.find().sort("group_name", 1)
-    return render_template("add_reservation.html", profiles=profiles)
+    reservations = mongo.db.reservation.find().sort("group_name", 1)
+    return render_template("add_reservation.html", reservations=reservations)
+
+
+@app.route('edit_reservation<edit_reservation>', methods=["GET", "POST"])
+def edit_reservation():
+    if method.request == "POST":
+        updated_reservation = {"$set": 
+        {
+            "group_name": request.form.get('group_name'),
+            "arrival_date": request.form.get('arrival_date'), 
+            "contact_name": request.form.get('contact_name'), 
+            "contact_email": request.form.get('contact_email'), 
+            "contact_phone": request.form.get('contact_phone'), 
+            "line_address": request.form.get('line_address'), 
+            "city": request.form.get('city'),
+            "postcode": request.form.get('postcode'),
+            "country": request.form.get('country'),
+            "los": request.form.get('los'), 
+            "status": request.form.get('status'), 
+            "board": request.form.get('board'), 
+            "single_room": request.form.get('single_room'),
+            "double_room": request.form.get('double_room'),
+            "twin_room": request.form.get('twin_room'),
+            "triple_room": request.form.get('triple_room'),
+            "single_rate": request.form.get('single_rate'),
+            "double_rate": request.form.get('double_rate'),
+            "twin_rate": request.form.get('twin_rate'),
+            "triple_rate": request.form.get('triple_rate'),
+            "notes": request.form.get('notes'),
+        }
+        }
+        mongo.db.reservations.update_one({"_id": ObjectId(reservation_id)}, uupdated_reservation))
+        return redirect(url_for("reservations"))
     
+    reservations = mongo.db.reservation.find().sort("group_name", 1)
+    return render_template("add_reservation.html", reservations=reservations)
+
 
 @app.route('/delete_reservation/<reservation_id>')
 def delete_reservation(reservation_id):
