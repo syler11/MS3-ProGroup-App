df5412b053647e4fc591a171ab438dbcf77cb3c1
syler11 <nemeth.szilard82@gmail.com>
2022-02-03 07:04:49 +0000

    progroup init.py is setup

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import (
     Flask, flash, render_template, redirect, request, session, url_for, Blueprint)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-import requests
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
---
+++ progroup/_init_.py
@@ -0,0 +1,35 @@
+import os
+from flask import (Flask)
+from flask_pymongo import PyMongo
+
+if os.path.exists("env.py"):
+    import env
+
+# App variables for setup and mongodb connectivity
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+mongo.init_app(app)
+
+
+def create_app():
+    """
+    Create an app with the authentication, errors, profiles, reservations, users blueprint routes
+    """
+    # Import the routes
+    from progroup.authentication.routes import authentication
+    from progroup.errors.routes import errors
+    from progroup.profiles.routes import profiles
+    from progroup.reservations.routes import reservations
+    from progroup.users.routes import users
+    # Register the routes with the app
+    app.register_blueprint(authentication)
+    app.register_blueprint(errors)
+    app.register_blueprint(profiles)
+    app.register_blueprint(reservations)
+    app.register_blueprint(users)
+    # Return the app
+    return app
